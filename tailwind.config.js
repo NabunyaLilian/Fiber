f2a7b5ea69e7b65e6dd23fd693e2b331dc3bcdbe
Nabunya Lilian <liliannabunya2@gmail.com>
2022-04-07 20:37:55 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: { 
+      colors:{
+        'dull-black': '#4F4F4F' ,
+        'blue-2': '#533FD1',
+      },
+    },
+  },
+  plugins: [],
+}
